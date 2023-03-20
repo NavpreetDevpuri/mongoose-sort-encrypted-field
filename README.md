11967fd12875de50e1185312225a5c64f2dc5622
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2023-03-20 17:06:42 +0530

    Added README and other middlewares. and refactored

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # mongoose-sort-encrypted-field
+
 Mongoose plugin to enable sorting on encrypted fields
+
+# Install
+
+```
+npm i mongoose-sort-encrypted-field
+```
+
+# Example
+
+## encryption.js
+```javascript
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const decrypt = function (encrypted) {
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+};
+
+const encrypt = function (plain) {
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(plain, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+};
+
+module.exports = {
+    decrypt,
+    encrypt
+}
+```
+## user.js
+We are having user with encrypted email
+
+```javascript
+const { encrypt, decrypt } = require('./encryption.js');
+const {
+  sortEncryptedFields,
+  evaluateMissedSortFields,
+} = require('mongoose-sort-encrypted-field');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    set: encrypt,
+    get: decrypt,
+    sortFieldName: 'emailSort',
+  },
+});
+
+userSchema.plugin(sortEncryptedFields);
+const User = mongoose.model('User', userSchema);
+
+// Sometimes some field could be missed due to server crash or anything so, handle those.
+evaluateMissedSortFields(User);
+
+module.exports = User;
+```
+
+Then we can sort all records by email from 'emailSort' field. For proformace we can create mongoDB index for that field.
+
+```javascript
+const await sortedUsers = await User.find({}).sort({ emailSort: 1 }).exec();
+```
+
+# How does it work?
+
+We create a sort order ID which is just a number in base 2^16, which is huge number system as compared to 10 base number system. We search in DB similar way to binary search but dividing the records into 100 parts intead of 2 parts in binary search. It generate sort order ID in O(1). 
--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,143 @@
 const Base2N = require('@navpreetdevpuri/base-2-n');
 
-function documentsBinarySearch(documents, fieldName, decrypter, value) {
-  let start = 0;
-  let end = documents.length - 1;
-
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    const decryptedMidValue = decrypter(
-      documents[mid][fieldName]
-    ).toLowerCase();
-    if (value < decryptedMidValue) {
-      end = mid - 1;
-    } else if (decryptedMidValue <= value) {
-      start = mid + 1;
-    }
-  }
-
-  return start;
-}
-
-function getAverageSortId(
-  predecessorSortId,
-  successorSortId,
-  numberOfCharsToIncreaseOnSaturation
+function sortEncryptedFields(
+  schema: {
+    post: Function;
+    add: Function;
+    paths: {
+      [fieldName: string]: {
+        options: { get: Function; sortFieldName: string };
+      };
+    };
+  },
+  options
 ) {
-  if (!predecessorSortId) {
-    predecessorSortId = ''.padEnd(successorSortId.length, '\0');
-  }
+  const {
+    noOfDividePartsForSearching = 100,
+    noOfCharsToIncreaseOnSaturation = 2,
+  } = options;
+  options.noOfDividePartsForSearching = noOfDividePartsForSearching;
+  options.noOfCharsToIncreaseOnSaturation = noOfCharsToIncreaseOnSaturation;
 
-  if (!successorSortId) {
-    successorSortId = ''.padEnd(predecessorSortId.length, '\uffff');
-  }
+  const sortFields: { [key: string]: string } = {};
+  const decrypters = {};
 
-  let predecessorNumber;
-  let successorNumber;
+  function documentsBinarySearch(documents, fieldName, value) {
+    let start = 0;
+    let end = documents.length - 1;
 
-  if (predecessorSortId.length == predecessorSortId.length) {
-    predecessorNumber = new Base2N(predecessorSortId);
-    successorNumber = new Base2N(successorSortId);
-    const averageNumber = predecessorNumber.average(successorNumber);
-    if (averageNumber.toString() != predecessorNumber.toString()) {
-      return averageNumber.toString();
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+      const decryptedMidValue = decrypters[fieldName](
+        documents[mid][fieldName]
+      ).toLowerCase();
+      if (value < decryptedMidValue) {
+        end = mid - 1;
+      } else if (decryptedMidValue <= value) {
+        start = mid + 1;
+      }
     }
-    predecessorNumber = new Base2N(
-      predecessorSortId.padEnd(
-        averageNumber.length + numberOfCharsToIncreaseOnSaturation,
-        '\0'
-      )
-    );
-    successorNumber = new Base2N(
-      successorSortId.padEnd(
-        averageNumber.length + numberOfCharsToIncreaseOnSaturation,
-        '\0'
-      )
-    );
-    return predecessorNumber.average(successorNumber).toString();
+
+    return start;
   }
 
-  const bigger =
-    predecessorSortId.length > successorSortId.length
-      ? predecessorSortId
-      : successorSortId;
-  const smaller =
-    successorSortId.length > predecessorSortId.length
-      ? predecessorSortId
-      : successorSortId;
+  function getAverageSortId(predecessorSortId, successorSortId) {
+    if (!predecessorSortId) {
+      predecessorSortId = ''.padEnd(successorSortId.length, '\0');
+    }
 
-  const biggerNumber = new Base2N(bigger);
-  const smallerNumber = new Base2N(smaller.padEnd(bigger.length, '\0'));
+    if (!successorSortId) {
+      successorSortId = ''.padEnd(predecessorSortId.length, '\uffff');
+    }
 
-  return biggerNumber.average(smallerNumber).toString();
-}
+    let predecessorNumber;
+    let successorNumber;
 
-function sortEncryptedFields(noOfDivideParts = 100) {
-  async function updateSortIdsForSave(
+    if (predecessorSortId.length == predecessorSortId.length) {
+      predecessorNumber = new Base2N(predecessorSortId);
+      successorNumber = new Base2N(successorSortId);
+      const averageNumber = predecessorNumber.average(successorNumber);
+      if (averageNumber.toString() != predecessorNumber.toString()) {
+        return averageNumber.toString();
+      }
+      predecessorNumber = new Base2N(
+        predecessorSortId.padEnd(
+          averageNumber.length + noOfCharsToIncreaseOnSaturation,
+          '\0'
+        )
+      );
+      successorNumber = new Base2N(
+        successorSortId.padEnd(
+          averageNumber.length + noOfCharsToIncreaseOnSaturation,
+          '\0'
+        )
+      );
+      return predecessorNumber.average(successorNumber).toString();
+    }
+
+    const bigger =
+      predecessorSortId.length > successorSortId.length
+        ? predecessorSortId
+        : successorSortId;
+    const smaller =
+      successorSortId.length > predecessorSortId.length
+        ? predecessorSortId
+        : successorSortId;
+
+    const biggerNumber = new Base2N(bigger);
+    const smallerNumber = new Base2N(smaller.padEnd(bigger.length, '\0'));
+
+    return biggerNumber.average(smallerNumber).toString();
+  }
+
+  async function getMatchForAggregate(
+    documents,
+    fieldName,
+    fieldValue,
+    sortFieldName
+  ) {
+    const index = documentsBinarySearch(documents, fieldName, fieldValue);
+    const gteIndex = index - 1;
+    const lteIndex = index;
+    const match = {
+      $and: [
+        { [sortFieldName]: { $ne: null } },
+        {
+          [sortFieldName]: {
+            $gte:
+              gteIndex == -1 ? undefined : documents[gteIndex][sortFieldName],
+            $lte:
+              lteIndex == documents.length
+                ? undefined
+                : documents[lteIndex][sortFieldName],
+          },
+        },
+      ],
+    };
+
+    return match;
+  }
+
+  async function updateSortFieldsForDocument(
+    objectId,
     model,
     fieldName,
     fieldValue,
-    sortIdFieldName,
-    objectId,
-    decrypters
+    sortFieldName
   ) {
     console.time(`Total time took to update orderId: ${objectId}`);
+    const pipeline = [];
     let currN = Math.round(
-      (await model.find({}).count().exec()) / noOfDivideParts
+      (await model.find({}).count().exec()) / noOfDividePartsForSearching
     );
-    const match: { $and: [{}] } = {
-      $and: [{ [sortIdFieldName]: { $ne: null } }],
-    };
+    let match: {} = { $and: { [sortFieldName]: { $ne: null } } };
     let documents;
-    console.time(`Total time took to search final documents: ${objectId}`);
-
     while (true) {
       documents = await model.aggregate([
         { $match: match },
         {
           $setWindowFields: {
-            sortBy: { [sortIdFieldName]: 1 },
+            sortBy: { [sortFieldName]: 1 },
             output: {
               index: { $rank: {} },
             },
@@ -109,139 +150,184 @@ function sortEncryptedFields(noOfDivideParts = 100) {
             },
           },
         },
-        { $project: { [fieldName]: 1, [sortIdFieldName]: 1 } },
+        { $project: { [fieldName]: 1, [sortFieldName]: 1 } },
       ]);
 
-      if (currN < noOfDivideParts) {
-        // documents.forEach((document) => (document[fieldName] = decrypters[fieldName](document[fieldName]).toLowerCase()));
+      if (currN < noOfDividePartsForSearching) {
         break;
       }
-
-      const index = documentsBinarySearch(
+      match = await getMatchForAggregate(
         documents,
         fieldName,
-        decrypters[fieldName],
-        fieldValue
+        fieldValue,
+        sortFieldName
       );
-      const gteIndex = index - 1;
-      const lteIndex = index;
-      match.$and.push({
-        [sortIdFieldName]: {
-          $gte:
-            gteIndex == -1 ? undefined : documents[gteIndex][sortIdFieldName],
-          $lte:
-            lteIndex == documents.length
-              ? undefined
-              : documents[lteIndex][sortIdFieldName],
-        },
-      });
-      // documents.forEach(
-      //   (document) =>
-      //     (document[fieldName] = decrypters[fieldName](
-      //       document[fieldName]
-      //     ).toLowerCase())
-      // );
-      currN = Math.round(currN / noOfDivideParts);
+      currN = Math.round(currN / noOfDividePartsForSearching);
     }
 
-    console.timeEnd(`Total time took to search final documents: ${objectId}`);
-
-    console.time(`Total time took to biinary serach: ${objectId}`);
-    const index = documentsBinarySearch(
-      documents,
-      fieldName,
-      decrypters[fieldName],
-      fieldValue
-    );
-    console.timeEnd(`Total time took to biinary serach: ${objectId}`);
-
-    console.time(`Total time took to get sortId: ${objectId}`);
-    const newSortId = getAverageSortId(
-      documents[index - 1][sortIdFieldName],
-      documents[index][sortIdFieldName],
-      1
-    );
-    console.timeEnd(`Total time took to get sortId: ${objectId}`);
-
-    console.time(`Total time took to get updateOne: ${objectId}`);
+    const index = documentsBinarySearch(documents, fieldName, fieldValue);
+    let gteIndex = index - 1;
+    let lteIndex = index;
+    const predecessorSortId = documents[gteIndex][sortFieldName];
+    const successorSortId = documents[lteIndex][sortFieldName];
+    const newSortId = getAverageSortId(predecessorSortId, successorSortId);
     await model.updateOne(
       { _id: objectId },
-      { $set: { [sortIdFieldName]: newSortId.toString() } }
+      { $set: { [sortFieldName]: newSortId.toString() } }
     );
-    console.timeEnd(`Total time took to get updateOne: ${objectId}`);
-
+    const documentsCountWithSameSortId = await model
+      .find({ [sortFieldName]: newSortId.toString() })
+      .count()
+      .exec();
+    if (documentsCountWithSameSortId > 1) {
+      const documentsWithSameSortId = await model
+        .find(
+          { [sortFieldName]: newSortId.toString() },
+          { _id: 1, [fieldName]: 1 }
+        )
+        .exec();
+      console.log(
+        `mongoose-sort-encrypted-field -> Got collions, retrying... ${documentsWithSameSortId}`
+      );
+      for (const document of documentsWithSameSortId) {
+        // Retrigering sortId generation due to collion
+        await model.updateOne(
+          { _id: document._id },
+          { $set: { [fieldName]: document[fieldName] } }
+        );
+      }
+    }
     console.timeEnd(`Total time took to update orderId: ${objectId}`);
   }
 
-  async function updateSortIdsForUpdateOne(
+  async function updateSortFieldsForUpdateOne(
+    filter,
     model,
     fieldName,
     fieldValue,
-    sortIdFieldName,
-    filter,
-    decrypters
+    sortFieldName
   ) {
-    // implementation details for updating the sorting ids for a specific field and document on updateOne go here
-    console.log(
-      `Updating sorting ids for ${fieldName} ${sortIdFieldName} and filter ${JSON.stringify(
-        filter
-      )} on updateOne`
-    );
+    const document = await model.findOne(filter, { _id: 1 }).exec();
+    if (document) {
+      await updateSortFieldsForDocument(
+        document._id,
+        model,
+        fieldName,
+        fieldValue,
+        sortFieldName
+      );
+    }
   }
 
-  return function (
-    schema: {
-      post: Function;
-      add: Function;
-      paths: {
-        [fieldName: string]: {
-          options: { get: Function; sortIdFieldName: string };
-        };
-      };
-    },
-    options
+  async function updateSortFieldsForUpdateMany(
+    filter,
+    model,
+    fieldName,
+    fieldValue,
+    sortFieldName
   ) {
-    const sortIdFields = {};
-    const decrypters = {};
-    for (const [fieldName, field] of Object.entries(schema.paths)) {
-      if (field.options.sortIdFieldName) {
-        sortIdFields[fieldName] = field.options.sortIdFieldName;
-        decrypters[fieldName] = field.options.get;
-        schema.add({
-          [field.options.sortIdFieldName]: {
-            type: String,
-            default: null,
-          },
-        });
+    const documents = await model.find(filter, { _id: 1 }).exec();
+    if (documents && documents.length > 0) {
+      for (let i = 0; i < documents.length; i += 1) {
+        await updateSortFieldsForDocument(
+          documents[i]._id,
+          model,
+          fieldName,
+          fieldValue,
+          sortFieldName
+        );
       }
     }
+  }
 
-    schema.post('save', async function (doc, next) {
-      for (const [fieldName, sortIdFieldName] of Object.entries(sortIdFields)) {
-        // if (doc.isModified(fieldName)) {
-        updateSortIdsForSave(
+  for (const [fieldName, field] of Object.entries(schema.paths)) {
+    if (field.options.sortFieldName) {
+      sortFields[fieldName] = field.options.sortFieldName;
+      decrypters[fieldName] = field.options.get;
+      schema.add({
+        [field.options.sortFieldName]: {
+          type: String,
+          default: null,
+        },
+      });
+    }
+  }
+
+  options.sortFields = sortFields;
+  options.decrypters = decrypters;
+
+  schema.post('save', async function (doc, next) {
+    for (const [fieldName, sortFieldName] of Object.entries(sortFields)) {
+      updateSortFieldsForDocument(
+        doc._id,
+        this.constructor,
+        fieldName,
+        doc[fieldName],
+        sortFieldName
+      );
+    }
+    next();
+  });
+
+  schema.post('updateOne', function (res, next) {
+    const update: { $set: { [key: string]: string } } = this.getUpdate();
+    for (const [fieldName, sortFieldName] of Object.entries(sortFields)) {
+      if (update.$set && update.$set[sortFieldName]) {
+        // Bypass middleware internal call for updating any sortFieldName field
+        break;
+      }
+      if (update.$set && update.$set[fieldName]) {
+        updateSortFieldsForUpdateOne(
+          this.getFilter(),
           this.constructor,
           fieldName,
-          doc[fieldName],
-          sortIdFieldName,
-          doc._id,
-          decrypters
+          update.$set[fieldName],
+          sortFieldName
         );
-        // }
       }
-      next();
-    });
+    }
+    next();
+  });
 
-    // schema.post('updateOne', async (res, next) => {
-    //   const update = res.getUpdate();
-    //   for (const [fieldName, sortIdName] of Object.entries(sortIdFields)) {
-    //     if (update.$set && update.$set[fieldName]) {
-    //       updateSortIdsForUpdateOne(this.constructor, fieldName, update.$set[fieldName], sortIdName, res.getFilter(), decrypters);
-    //     }
-    //   }
-    //   next();
-    // });
-  };
+  schema.post('updateMany', function (res, next) {
+    const update = this.getUpdate();
+    for (const fieldName of Object.keys(sortFields)) {
+      const sortFieldName = sortFields[fieldName];
+      if (update.$set && update.$set[sortFieldName]) {
+        // Bypass middleware internal call for updating any sortFieldName field
+        break;
+      }
+      if (update.$set && update.$set[fieldName]) {
+        updateSortFieldsForUpdateMany(
+          this.getFilter(),
+          this.constructor,
+          fieldName,
+          update.$set[fieldName],
+          sortFieldName
+        );
+      }
+    }
+    next();
+  });
 }
 
-module.exports = sortEncryptedFields;
+async function evaluateMissedSortFields(model) {
+  const pluginOptions = model.schema.plugins.find(
+    (plugin) => plugin.fn.name == 'sortEncryptedFields'
+  );
+  if (!pluginOptions) {
+    throw 'Plugin is not enabled on this model, Try ModelSchema.plugin(sortEncryptedFields), brefore creating Model.';
+  }
+  for (const fieldName of Object.keys(pluginOptions.sortFields)) {
+    const sortFieldName = pluginOptions.sortFields[fieldName];
+    const documents = await model.find({ [sortFieldName]: { $eq: null } });
+    for (const document of documents) {
+      // Retrigering sortId generation
+      await model.updateOne(
+        { _id: document._id },
+        { $set: { [fieldName]: document[fieldName] } }
+      );
+    }
+  }
+}
+module.exports = { sortEncryptedFields, evaluateMissedSortFields };
