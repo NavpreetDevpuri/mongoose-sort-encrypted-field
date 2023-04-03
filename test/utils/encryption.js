67251af94cb55821a8821a01056c5f9f82ee8ac2
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2023-04-03 16:55:51 +0530

    reverted encrytions.js

--- test/utils/encryption.js
+++ test/utils/encryption.js
@@ -5,7 +5,6 @@ const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 
 function decrypt(encrypted) {
-  return encrypted;
   const decipher = crypto.createDecipheriv(algorithm, key, iv);
   let decrypted = decipher.update(encrypted, "hex", "utf8");
   decrypted += decipher.final("utf8");
@@ -13,7 +12,6 @@ function decrypt(encrypted) {
 };
 
 function encrypt(plain) {
-  return plain;
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(plain, "utf8", "hex");
   encrypted += cipher.final("hex");
